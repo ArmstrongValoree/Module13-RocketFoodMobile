cd8b5ece47c4f6181633e71c6c2431e6799c6597
Valoree Armstrong <armstrongvaloree@gmail.com>
2026-04-17 16:01:36 -0400

    fix: break circular dependency with @Lazy on JwtTokenFilter injection

--- server/src/main/java/com/rocketFoodDelivery/rocketFood/security/SecurityConfig.java
+++ server/src/main/java/com/rocketFoodDelivery/rocketFood/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +26,7 @@ public class SecurityConfig {
     @Autowired
     UserRepository userRepository;
 
+    @Lazy
     @Autowired
     JwtTokenFilter jwtTokenFilter;
 
